2ce123b4d84dc60d6cd47ac06d49f3bc77abe746
Takashi Asaba <nelnal@gmail.com>
2023-05-16 19:36:18 +0900

    format(wezterm): format wezterm.lua
    SSIA

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,104 +1,108 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 keys = {
   -- with leader
   {
-    key = 'c',
-    mods = 'LEADER',
-    action = act.SpawnTab 'DefaultDomain',
+    key = "c",
+    mods = "LEADER",
+    action = act.SpawnTab("DefaultDomain"),
   },
   {
-    key = 'n',
-    mods = 'LEADER',
+    key = "n",
+    mods = "LEADER",
     action = act.ActivateTabRelative(1),
   },
   {
-    key = 'p',
-    mods = 'LEADER',
+    key = "p",
+    mods = "LEADER",
     action = act.ActivateTabRelative(-1),
   },
   {
-    key = 'r',
-    mods = 'LEADER',
-    action = act.PromptInputLine {
-      description = 'Enter new name for tab',
+    key = "r",
+    mods = "LEADER",
+    action = act.PromptInputLine({
+      description = "Enter new name for tab",
       action = wezterm.action_callback(function(window, pane, line)
         if line then
           window:active_tab():set_title(line)
         end
-      end)
-    },
+      end),
+    }),
   },
   {
-    key = 's',
-    mods = 'LEADER',
+    key = "s",
+    mods = "LEADER",
     action = act.ActivateTabRelative(-1),
   },
   {
-    key = '|',
-    mods = 'LEADER',
-    action = act.SplitHorizontal {},
+    key = "|",
+    mods = "LEADER",
+    action = act.SplitHorizontal({}),
   },
   {
-    key = '-',
-    mods = 'LEADER',
-    action = act.SplitVertical {},
+    key = "-",
+    mods = "LEADER",
+    action = act.SplitVertical({}),
   },
   {
-    key = '#',
-    mods = 'LEADER',
-    action = act.SplitPane {
-      direction = 'Down',
+    key = "#",
+    mods = "LEADER",
+    action = act.SplitPane({
+      direction = "Down",
       size = { Cells = 20 },
-    },
+    }),
   },
   {
-    key = 'LeftArrow',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivatePaneDirection 'Left',
+    key = "LeftArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Left"),
   },
   {
-    key = 'RightArrow',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivatePaneDirection 'Right',
+    key = "RightArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Right"),
   },
   {
-    key = 'UpArrow',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivatePaneDirection 'Up',
+    key = "UpArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Up"),
   },
   {
-    key = 'DownArrow',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivatePaneDirection 'Down',
+    key = "DownArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Down"),
   },
 
   -- with leader + CTRL
   {
-    key = 'd',
-    mods = 'LEADER|CTRL',
-    action = act.CloseCurrentTab { confirm = true },
+    key = "d",
+    mods = "LEADER|CTRL",
+    action = act.CloseCurrentTab({ confirm = true }),
   },
 }
 for i = 0, 9 do
   table.insert(keys, {
     key = tostring(i),
-    mods = 'LEADER',
+    mods = "LEADER",
     action = act.ActivateTab(i),
   })
 end
 
 return {
-  color_scheme = 'Dark+',
+  color_scheme = "Dark+",
+  colors = {
+    scrollbar_thumb = "#DDDDDD",
+  },
   enable_scroll_bar = true,
-  font = wezterm.font('Hack Nerd Font Mono'),
-  font_size = 15.0,
+  font = wezterm.font("Hack Nerd Font Mono"),
+  font_size = 17.0,
   keys = keys,
   leader = {
-    key = 't',
-    mods = 'CTRL',
+    key = "t",
+    mods = "CTRL",
   },
+  scrollback_lines = 10000,
   tab_and_split_indices_are_zero_based = true,
   tab_bar_at_bottom = true,
   use_ime = true,
